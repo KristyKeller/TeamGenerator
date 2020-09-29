ac548b49f26362f7b30f2f526d96ba5b0c72f9a4
Kristy Keller <keller.kristy@outlook.com>
2020-09-28 20:59:09 -0400

    all tests passed

--- app.js
+++ app.js
@@ -12,7 +12,118 @@ const render = require("./lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// and to create objects for each team member using classes
+
+const employees = [];
+// inquirer.prompt([
+
+//     // Inquirer Prompt #1
+//     // Questions about the manager 
+//     console.log("Please build your team"),
+//     {
+//         type: "input",
+//         message: "What is your manager's name?",
+//         name: "managerName"
+//     },
+//     {
+//         type: "input",
+//         message: "What is your manager's id?",
+//         name: "managerId"
+//     },
+//     {
+//         type: "input",
+//         message: "What is your manager's email?",
+//         name: "managerEmail"
+//     },
+//     {
+//         type: "input",
+//         message: "What is your manager's office number?",
+//         name: "managerOfficeNumber"
+//     }
+//         .then(function (managerAnswers) {
+//             const manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber)
+//             employees.push(manager)
+
+//             // Inquirer Prompt #2
+//             // Select your team member 
+
+//             {
+//                 type: "list",
+//                     message: "Which type of team member would you like to add?",
+//                         name: "license",
+//                             choices: [
+//                                 "Engineer",
+//                                 "Intern",
+//                                 "No more team members"
+//                             ]
+//             }
+// .then(typeAnswers) {
+//             if(typeAnswers === "Engineer") }
+//     // Inquirer Prompt #3
+//     // Questions about the engineer 
+//     {
+//         type: "input",
+//         message: "What is your engineer's name?",
+//         name: "engineerName"
+//     },
+//     {
+//         type: "input",
+//         message: "What is your engineer's id?",
+//         name: "engineerId"
+//     },
+//     {
+//         type: "input",
+//         message: "What is your engineer's email?",
+//         name: "engineerEmail"
+//     },
+//     {
+//         type: "input",
+//         message: "What is your engineer's Github?",
+//         name: "engineerGithub"
+//     }
+//         .then(function (engineerAnswers) {
+//             const engineer = new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.github)
+//             employees.push(engineer)
+//         }
+//     } else if (typeAnswers === "Intern") {
+//     // Inquirer Prompt #4
+//     // Questions about the intern
+//     {
+//         type: "input",
+//             message: "What is your intern's name?",
+//                 name: "internName"
+//     },
+//     {
+//         type: "input",
+//             message: "What is your intern's id?",
+//                 name: "internId"
+//     },
+//     {
+//         type: "input",
+//             message: "What is your intern's email?",
+//                 name: "internEmail"
+//     },
+//     {
+//         type: "input",
+//             message: "What is your intern's Github?",
+//                 name: "internSchool"
+//     } 
+//  .then(function (internAnswers) {
+//         const intern = new Intern(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.officeSchool)
+//     } else {
+//     Exit the inquirer
+//     const html = render(employees)
+//     fs.writeFile(outputPath, html, function (err) {
+//         if (err)
+//             throw err;
+
+//         console.log("Success!");
+//     });
+// }])
+
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
